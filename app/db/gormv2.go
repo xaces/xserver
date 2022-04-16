35a0306d5d3e5ca5bb378afed7f85f37e9dde99e
don.wang <wanguandong@126.com>
2022-04-16 11:05:40 +0800

    perf: 使用默认orm初始化

--- app/db/gormv2.go
+++ app/db/gormv2.go
@@ -1,31 +1,9 @@
 package db
 
 import (
-	"errors"
 	"xserver/model"
 
 	"github.com/wlgd/xutils/orm"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-	"gorm.io/gorm/schema"
-)
-
-func initTables(db *gorm.DB) {
-	db.AutoMigrate(&model.SysMenu{}, &model.SysRole{}, &model.SysUser{},
-		&model.SysDictType{}, &model.SysDictData{}, &model.SysDept{}, &model.SysPost{}, &model.SysFile{}, &model.SysTation{})
-	db.AutoMigrate(&model.OprOrganization{}, &model.OprVehicle{})
-	orm.SetDB(db.Debug())
-}
-
-var (
-	gconf = gorm.Config{
-		NamingStrategy: schema.NamingStrategy{
-			SingularTable: true,
-		},
-	}
-	db *gorm.DB
 )
 
 type Options struct {
@@ -35,30 +13,25 @@ type Options struct {
 
 // Run 初始化服务
 func Run(o *Options) error {
-	switch o.Name {
-	case "sqlite":
-		db, _ = gorm.Open(sqlite.Open(o.Address), &gconf)
-	case "mysql":
-		db, _ = gorm.Open(mysql.New(mysql.Config{
-			DSN: o.Address,
-			// DefaultStringSize:         64,    // string 类型字段的默认长度
-			DisableDatetimePrecision:  true,  // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
-			DontSupportRenameIndex:    true,  // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
-			DontSupportRenameColumn:   true,  // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
-			SkipInitializeWithVersion: false, // 根据版本自动配置
-		}), &gconf)
-	}
-	if db == nil {
-		return errors.New("db invalid")
-	}
-	sqldb, err := db.DB()
+	db, err := orm.NewGormV2(o.Name, o.Address)
 	if err != nil {
 		return err
 	}
-	// SetMaxIdleCons 设置连接池中的最大闲置连接数。
-	sqldb.SetMaxIdleConns(10)
-	// SetMaxOpenCons 设置数据库的最大连接数量。
-	sqldb.SetMaxOpenConns(100)
-	initTables(db)
+	db.AutoMigrate(
+		&model.SysMenu{},
+		&model.SysRole{},
+		&model.SysUser{},
+		&model.SysDictType{},
+		&model.SysDictData{},
+		&model.SysDept{},
+		&model.SysPost{},
+		&model.SysFile{},
+		&model.SysTation{},
+	)
+	db.AutoMigrate(
+		&model.OprOrganization{},
+		&model.OprVehicle{},
+	)
+	orm.SetDB(db.Debug())
 	return nil
 }
--- go.mod
+++ go.mod
@@ -14,9 +14,6 @@ require (
 	github.com/satori/go.uuid v1.2.0
 	github.com/unrolled/secure v1.0.9
 	github.com/wlgd/xutils v0.0.0-20210805011628-fdcc6c9015ec
-	gorm.io/driver/mysql v1.1.2
-	gorm.io/driver/sqlite v1.1.4
-	gorm.io/gorm v1.21.12
 )
 
 replace github.com/wlgd/xutils => ../xutils
